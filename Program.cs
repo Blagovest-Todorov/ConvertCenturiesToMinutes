f6ded2277d490753e619d64a3eb539239f10850c
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-20 10:04:39 +0200

    add the correct file Program.cs for calculating centutiest to minutes

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Numerics;
 
-namespace DataTypeTutorials
+namespace PoundsDollars
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int meters = int.Parse(Console.ReadLine());  //we read the input in meters
-            float kilometers = meters / 1000.0F;
-            Console.WriteLine($"{kilometers:F2}");
+            byte inputNum = byte.Parse(Console.ReadLine());
+            // centuries
+            ushort years = (ushort)(100 * inputNum);         // to years
+            int days = (int)(years * 365.2422);                // to days
+            uint hours = (uint)days * 24;                       // to hours
+            long minutes = hours * 60;                    // to minutes
 
+            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes",
+                inputNum, years, days, hours, minutes);
         }
     }
 }
